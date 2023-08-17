258eb7466d506d9221b80e242aafc77a1bc48046
ozgucdlg <ozgucdalga@gmail.com>
2023-08-17 07:51:51 -0400

    Initial commit

---
+++ src/main/java/com/portfolio/personalPortfolio/PersonalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.personalPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/personalPortfolio/PersonalPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.personalPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
